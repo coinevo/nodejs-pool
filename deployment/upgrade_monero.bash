38c6ed797568a4cac2f6094e079976d347f89b75
coinevo <esther4benson12@gmail.com>
2020-02-17 17:34:15 +0100

    changed monero-->coinevo

--- deployment/upgrade_monero.bash
+++ deployment/upgrade_monero.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
-echo "This assumes that you have a standard nodejs-pool install, and will patch and update it to the latest stable builds of Monero."
+echo "This assumes that you have a standard nodejs-pool install, and will patch and update it to the latest stable builds of Coinevo."
 sleep 15
 echo "Continuing install, this will prompt you for your password if you didn't enable passwordless sudo.  Please do not run me as root!"
-cd /usr/local/src/monero &&\
+cd /usr/local/src/coinevo &&\
 sudo git checkout .  &&\
 sudo git checkout master &&\
 sudo git pull &&\
-sudo git checkout v0.15.0.1 &&\
+sudo git checkout master &&\
 sudo git submodule init &&\
 sudo git submodule update &&\
 sudo rm -rf build &&\
 (sudo USE_SINGLE_BUILDDIR=1 make -j$(nproc) || sudo USE_SINGLE_BUILDDIR=1 make) &&\
-echo "Done building the new Monero daemon! Please go ahead and reboot monero with: sudo systemctl restart monero as soon as the pool source is updated!"
+echo "Done building the new Coinevo daemon! Please go ahead and reboot Coinevo with: sudo systemctl restart Coinevo as soon as the pool source is updated!"
